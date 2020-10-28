5002054fe29c59a305bbf746bc8af3e1dd64d8c4
Mr.Fang <2875616188@qq.com>
2020-10-28 09:24:45 +0800

    添加 站点配置信息服务.JS文件 Http请求服务文件.JS文件 宅音乐播放器.JS文件

---
+++ src/FH.Host.APP/wwwroot/assets/config/configService.js
@@ -0,0 +1,133 @@
+/*
+ * 站点配置信息服务
+ * Author：Mr.Fang
+ * QQ：2875616188
+ * Version：1.1.0
+ * WebSite：https://fanghua.host
+ * Time：2020.3.27
+ */
+
+/**配置信息 */
+var appConfiguration = {
+  // 站点信息
+  ProjectInfo: {
+    // 站点名称
+    Project_Name: "Mr.Fang网络",
+    // 站点版本
+    Version: "V1.1.0",
+    // 站点Url
+    Url: "https://fanghua.host",
+    // APIUrl
+    ApiRequestUrl: "https://api.fanghua.host",
+    // 备案信息
+    RecordInfo: "陇ICP备2020003856号",
+    // 提示框头部信息
+    InfoHeader: "【Mr.Fang网络】温馨提示：",
+  }
+};
+
+/**配置文件 */
+var appConfigFile = {
+  // JS文件信息
+  JSFileInfo: {
+    // 站点配置信息JS文件
+    AppConfigurationJSFile: this.appConfiguration.ProjectInfo.Url +
+      "/assets/config/configService.js",
+    // 默认jQuery文件
+    JQueryJSFile: this.appConfiguration.ProjectInfo.Url +
+      "/assets/plugins/jquery/jquery-3.5.1.min.js",
+    // Http请求服务配置JS文件
+    HttpRequestSeiverJSFile: this.appConfiguration.ProjectInfo.Url +
+      "/assets/httpService/httpRequestService.js",
+    // 音乐播放器信息
+    MusicsPlayerJS:
+      '<script id="ilt" key="98ffa0f27310403e85637d328e6ba248" src="' + this.appConfiguration.ProjectInfo.Url + '/assets/plugins/player/player.min.js" ></script>',
+    // Layer.js文件
+    LayerJSFile: this.appConfiguration.ProjectInfo.Url +
+      "/assets/plugins/layer/layer.js",
+    // 默认JS文件
+    DefaultJSFile: [],
+  }
+}
+
+var headHtml = document.getElementsByTagName("head").item(0);
+console.log(headHtml);
+
+var webSiteDate = new Date();
+
+// 设置Title加站点名称
+var webSiteTitle = document.getElementsByTagName("title").item(0);
+webSiteTitle.text =
+  appConfiguration.ProjectInfo.Project_Name + " - " + webSiteTitle.text;
+
+// 备案信息
+var webSiteFooter = document.getElementById("footer");
+webSiteFooter.innerHTML =
+  '<small class="copyright">Copyright&nbsp;&copy 2019 - ' + this.webSiteDate.getFullYear() + '&nbsp;&nbsp;<a href="https://fanghua.host">' + this.appConfiguration.ProjectInfo.Project_Name + '</a>&nbsp;&nbsp;版权所有&nbsp;&nbsp;|&nbsp;&nbsp;<a href="http://beian.miit.gov.cn/" target="_blank">' + this.appConfiguration.ProjectInfo.RecordInfo + '</a></small>';
+
+/**加载jQuery文件 */
+function loadJQuery() {
+  var script = document.createElement("script");
+  script.async = false;
+  //script.type = "text/javascript";
+  script.src = this.appConfigFile.JSFileInfo.JQueryJSFile;
+  headHtml.appendChild(script);
+  console.log("jQuery JS文件加载成功！");
+}
+
+/**加载Layer文件 */
+function loadLayer() {
+  // 加载Layer.JS默认加载jQuery.JS文件
+  this.loadJQuery();
+  var script = document.createElement("script");
+  script.async = false;
+  //script.type = "text/javascript";
+  script.src = this.appConfigFile.JSFileInfo.LayerJSFile;
+  headHtml.appendChild(script);
+  console.log("Layer JS文件加载成功！");
+}
+
+/**加载Http请求服务配置JS文件 */
+function loadHttpRequestServer() {
+  // 加载Http请求服务默认加载Layer.JS文件
+  this.loadLayer();
+  var script = document.createElement("script");
+  script.async = false;
+  //script.type = "text/javascript";
+  script.src = this.appConfigFile.JSFileInfo.HttpRequestSeiverJSFile;
+  headHtml.appendChild(script);
+  console.log("Http请求服务 JS文件加载成功！");
+}
+
+/**加载音乐播放器 */
+function loadMusicsPlayer() {
+  // 当访问设备为电脑或平板（宽度大于640）时再加载播放器
+  if (window.screen.width > 640) {
+    $("head").append(this.appConfigFile.JSFileInfo.MusicsPlayerJS);
+  }
+}
+
+/**方法封装 */
+/**
+ * 错误信息提示
+ * @param {any} msg
+ */
+function errorInfo(msg) {
+  layer.msg(msg);
+}
+/**
+ * 成功信息提示
+ * @param {any} msg
+ */
+function successInfo(msg) {
+  layer.msg(msg);
+}
+/**
+ * 得到随机数（包括上限和下限）
+ * @param {any} maxIndexOf 上限
+ * @param {any} minIndexOf 下限
+ */
+function getRandom(maxIndexOf, minIndexOf) {
+  // return parseInt(Math.random() * (maxIndexOf - minIndexOf) + minIndexOf);
+  return Math.floor(Math.random() * (maxIndexOf - minIndexOf + 1)) + minIndexOf;
+}
---
+++ src/FH.Host.APP/wwwroot/assets/httpService/httpRequestService.js
@@ -0,0 +1,102 @@
+/*
+ * Http请求服务文件
+ * Author：Mr.Fang
+ * QQ：2875616188
+ * Version：1.1.0
+ * WebSite：https://fanghua.host
+ * Time：2020.3.27
+ */
+
+/**
+ * HTTP请求服务
+ * @param {any} obj 请求方式、路径、参数。实例：obj: {method:"get",url:"",data:{},dataType:""};
+ * @param {any} successFun 请求成功回调方法
+ * @param {any} errorFun 请求失败回调方法
+ * @param {any} async 是否异步，默认异步
+ * @param {any} apiRequest 是否为API请求，默认为API请求
+ */
+function httpRequest(obj, successFun, errorFun, async = true, apiRequest = true) {
+    // 创建 XMLHttpRequest 对象
+    var xmlHttp = null;
+    // 老版本的 Internet Explorer （IE5 和 IE6）使用 ActiveX 对象：xmlhttp = new ActiveXObject("Microsoft.XMLHTTP")
+    if (window.XMLHttpRequest) {
+        // code for all new browsers
+        xmlHttp = new XMLHttpRequest;
+    } else if (window.ActiveXObject) {
+        // code for IES and IE6
+        xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    // 判断是否支持请求
+    if (xmlHttp == null) {
+        info = "浏览器不支持xmlHttp请求，请更换更高级的浏览器再试试。";
+        console.log(appConfiguration.ProjectInfo.InfoHeader + info);
+        errorInfo(appConfiguration.ProjectInfo.InfoHeader + info);
+    }
+    // 请求方式，转换为大写
+    var httpMethod = (obj.method || "Get").toUpperCase();
+    // 数据类型
+    xmlHttp.responseType = obj.dataType || "json";
+    // url
+    var httpUrl = obj.url || '';
+    // 判断是否为API请求
+    var httpApiRequest = apiRequest == null || apiRequest == undefined || apiRequest == '' && apiRequest != false ? true : apiRequest;
+    if (httpApiRequest) {
+        httpUrl = '' || appConfiguration.ProjectInfo.ApiRequestUrl + "/api/" + httpUrl;
+    } else {
+        httpUrl = '' || appConfiguration.ProjectInfo.Url + "/" + httpUrl;
+    }
+    // 异步请求
+    var httpAsync = async == null || async == undefined || async == '' && async != false ? true : async;
+    // get请求时参数处理
+    if (httpMethod == "GET") {
+        // 请求体中的参数get请求参数格式为：?paraml=test&param2=test2
+        var data = obj.data || {};
+        var requestData = '';
+        for (var key in data) {
+            requestData += key + "=" + data[key] + "&";
+        }
+        if (requestData != '') {
+            httpUrl += "?";
+            requestData = requestData.substring(0, requestData.length - 1);
+            httpUrl += requestData;
+        }
+    }
+    // onreadystatechange 是一个事件句柄。
+    // 它的值(state_Change) 是一个函数的名称，当 XMLHttpRequest 对象的状态发生改变时，会触发此函数。状态从 0(uninitialized) 到 4(complete) 进行变化。仅在状态为 4 时，我们才执行代码
+    xmlHttp.onreadystatechange = function () {
+        // complete
+        if (xmlHttp.readyState == 4) {
+            console.log(xmlHttp);
+            if (xmlHttp.status == 200) {
+                // 请求成功执行的回调函数
+                successFun(xmlHttp.response, xmlHttp);
+            } else {
+                // 请求失败执行的回调函数
+                errorFun(xmlHttp, xmlHttp.status, xmlHttp.statusText);
+            }
+        }
+    }
+    // 请求接口
+    if (httpMethod == "GET") {
+        xmlHttp.open("GET", httpUrl, httpAsync);
+        xmlHttp.send(null);
+    } else {
+        xmlHttp.open(httpMethod, httpUrl, httpAsync);
+        xmlHttp.setRequestHeader("Content-Type", "application/json");
+        xmlHttp.send(JSON.stringify(obj.data));
+    }
+}
+
+/**添加访问日志 */
+function addAccessHistoryLog() {
+    httpRequest({
+        method: "POST",
+        url: "AccessHistoryLogAppService/CreateAccessHistoryLog",
+        data: {
+            deviceInfo: navigator.userAgent,
+            url: currentUrlPath
+        }
+    }, function (res) {
+    }, function () {
+    });
+}
