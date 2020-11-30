c857060ba0dad7be4bcb7ad8f70b1384039589e8
Mr.Fang <2875616188@qq.com>
2020-11-30 19:30:56 +0800

    迁移项目为HW

---
+++ src/HW.Host.APP/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FH.Host.APP.Core
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            // 设置默认启动页为引导页面
+            DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            defaultFilesOptions.DefaultFileNames.Clear();
+            defaultFilesOptions.DefaultFileNames.Add("HW.Host.APP.GuidPage/GuidPageIndex.html");
+            app.UseDefaultFiles(defaultFilesOptions);
+
+            app.UseHttpsRedirection(); // 强制把HTTP请求转换HTTPS
+            app.UseStaticFiles(); // 配置静态文件中间件，可访问位于 Web 根目录之外的文件
+            app.UseCookiePolicy();
+        }
+    }
+}
